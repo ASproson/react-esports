661086a0bb22701bb84528aa2856397a70c0b76c
ASproson <a.sproson@outlook.com>
2022-08-10 17:25:42 +0100

    Adds white triangle cut out

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -11,6 +11,7 @@ const HeroBanner = () => {
         <div className="whiteBoxRight absolute top-20 translate-x-[1330px] border-[1.5px] border-white opacity-25 h-full -mt-12 z-10 w-[200px] shadow-2xl"></div>
         <div className="redBox absolute top-[150px] translate-x-[106px] bg-red-500 h-4 w-4"></div>
         <div className="redTriangle absolute top-[152px] translate-x-[116px] bg-red-500 h-3 w-3 rotate-45"></div>
+        <div className="whiteCutOut absolute bottom-[10px] -translate-x-[40px] bg-white h-20 w-20 rotate-45"></div>
         <div className="vidBackground">
           <div className="videoLoop">
             <ReactPlayer
