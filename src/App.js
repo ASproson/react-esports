e42f578b7572377ea0552505ff32a3608680f171
atlas <a.sproson@outlook.com>
2022-08-10 14:33:47 +0100

    Creates HeroBanner.jsx, calls inside App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import HeroBanner from './components/HeroBanner';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <h1>Hello World</h1>
+      <HeroBanner />
     </div>
   );
 }
