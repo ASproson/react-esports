456a9de482670c85b93a1d78d702323e37370f32
ASproson <a.sproson@outlook.com>
2022-08-10 17:12:32 +0100

    Adds secondary white line and better aligns previous white box

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -5,9 +5,10 @@ const HeroBanner = () => {
   const videoUrl =
     "https://assets.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt714eaee50b90fc27/62cc7dcc6a8fb133b0ff7e55/VALORANT_ANNO22_SHATTERED_16x9_27s.mp4";
   return (
-    <div className="border-red-500 border-2">
+    <div>
       <div className="-mt-44">
-        <div className="whiteBox absolute top-20 translate-x-24 border-[1.5px] border-white opacity-25 h-full -mt-12 z-10 w-[270px] shadow-2xl"></div>
+        <div className="whiteBox absolute top-20 translate-x-[105px] border-[1.5px] border-white opacity-25 h-full -mt-12 z-10 w-[315px] shadow-2xl"></div>
+        <div className="whiteBox absolute top-20 translate-x-[1330px] border-[1.5px] border-white opacity-25 h-full -mt-12 z-10 w-[200px] shadow-2xl"></div>
         <div className="vidBackground">
           <div className="videoLoop">
             <ReactPlayer
