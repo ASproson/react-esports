b51f8371fb6a62910ecda5073a39be7fcc7facf7
atlas <a.sproson@outlook.com>
2022-08-10 14:56:35 +0100

    Adds react-player dependency

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const HeroBanner = () => {
-    return (
-        <div>
-            Hero
+  const videoUrl = "https://youtu.be/e_E9W2vsRbQ";
+  return (
+    <div>
+      <div className="vidBackground">
+        <div className="videoLoop border-2 border-red-500">
+            
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default HeroBanner;
