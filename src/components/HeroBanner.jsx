7fdcf11b7ed10d9cd78ea31d84cd93e015bf4759
atlas <a.sproson@outlook.com>
2022-08-10 20:45:57 +0100

    Adds title text

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -15,18 +15,24 @@ const HeroBanner = () => {
 
         <div className="whiteCutOut absolute bottom-[95px] -translate-x-[45px] bg-white h-20 w-20 rotate-45"></div>
 
+        <div className="absolute top-1/3 transform left-1/2 -translate-x-1/2 ">
+          <h1 className="font-valorant text-white text-[90px]">
+            The Latest Events
+          </h1>
+          <h1 className="font-valorant text-white text-[20px]">
+            The place for tactical shooter news
+          </h1>
+        </div>
+
         <div className="whiteBracketsRedBoxTop left-1/2 transform -translate-x-1/2 absolute bottom-[47.5%] border-t-2 border-l-2 border-r-2 border-white opacity-70 h-[30px] w-[255px]"></div>
         <div className="whiteBracketsRedBoxBottom left-1/2 transform -translate-x-1/2 absolute bottom-[43.9%] border-b-2 border-l-2 border-r-2 border-white opacity-70 h-[30px] w-[255px]"></div>
         <div className="redBox absolute bottom-[43%] left-1/2 transform -translate-x-1/2 -translate-y-1/3 bg-red-500 h-[60px] w-[238px]">
           <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white uppercase font-bold text-sm">
             Watch Live
           </p>
-        <div className="smallBoxesWithinWatchLiveTopLeft absolute bg-black h-[3px] w-[3px]"></div>
-        <div className="smallBoxesWithinWatchLiveBottomRight absolute bottom-0 right-0 bg-black h-[3px] w-[3px]"></div>
-
+          <div className="smallBoxesWithinWatchLiveTopLeft absolute bg-black h-[3px] w-[3px]"></div>
+          <div className="smallBoxesWithinWatchLiveBottomRight absolute bottom-0 right-0 bg-black h-[3px] w-[3px]"></div>
         </div>
-
-
 
         <div className="vidBackground">
           <div className="videoLoop">
