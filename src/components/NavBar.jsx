8c2f3ed3e245b7eaf35ccc85c87a4f4671a0a967
atlas <a.sproson@outlook.com>
2022-08-10 14:27:42 +0100

    Completes first iteration of NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React from "react";
 
 const NavBar = () => {
-    return (
-        <div>
-            Nav
-        </div>
-    );
+  const navList = [
+    {
+      id: 1,
+      title: "schedule",
+    },
+    {
+      id: 2,
+      title: "watch",
+    },
+    {
+      id: 3,
+      title: "standings",
+    },
+    {
+      id: 4,
+      title: "news",
+    },
+    {
+      id: 5,
+      title: new Date().getFullYear() + 1 + " VCT",
+    },
+  ];
+
+  return (
+    <div className="navContainer flex bg-black h-20 justify-between sticky">
+      <div className="leftNav flex mr-6 text-white items-center ml-14">Logo</div>
+      <div className="rightNav flex space-x-6 text-[#8FA3B0] items-center mr-14 uppercase font-bold text-sm">
+        {navList.map((navEl) => {
+          return <p key={navEl.id}>{navEl.title}</p>;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
