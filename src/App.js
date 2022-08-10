a5dbc0c47b53466c5bad98ee6b898831a0393a04
atlas <a.sproson@outlook.com>
2022-08-10 14:27:26 +0100

    Calls NavBar.js before main content

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import NavBar from './components/NavBar';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
       <NavBar />
+      <h1>Hello World</h1>
     </div>
   );
 }
