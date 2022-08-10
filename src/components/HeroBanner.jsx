6449806030096a5fb946bfa09fbc1f9311e58fd2
atlas <a.sproson@outlook.com>
2022-08-10 20:21:01 +0100

    Correctly positions watch live button and surrounding style brackets

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -9,12 +9,15 @@ const HeroBanner = () => {
       <div className="-mt-44">
         <div className="whiteBoxLeft absolute top-20 left-[32%] transform -translate-x-1/2 border-[1.5px] border-white opacity-25 h-full -mt-12 z-10 w-[310px] shadow-2xl"></div>
         <div className="whiteBoxRight absolute top-20 transform -translate-x-1/2 right-[14%] border-l-[1.5px] border-white opacity-25 h-full -mt-12 z-10 w-[200px] shadow-2xl"></div>
+
         <div className="redBox absolute top-[150px] transform left-[26.2%] -translate-x-1/2 bg-red-500 h-4 w-4"></div>
         <div className="redTriangle absolute top-[152px] transform -translate-x-1/2 left-[26.5%] bg-red-500 h-3 w-3 rotate-45"></div>
+
         <div className="whiteCutOut absolute bottom-[95px] -translate-x-[45px] bg-white h-20 w-20 rotate-45"></div>
-        <div className="whiteBracketsRedBoxTop left-1/2 transform -translate-x-1/2 absolute bottom-[289px] border-t-2 border-l-2 border-r-2 border-white opacity-70 h-[30px] w-[255px]"></div>
-        <div className="whiteBracketsRedBoxBottom left-1/2 transform -translate-x-1/2 absolute bottom-[242px] border-b-2 border-l-2 border-r-2 border-white opacity-70 h-[30px] w-[255px]"></div>
-        <div className="redBox absolute bottom-[230px] left-1/2 transform -translate-x-1/2 -translate-y-1/3 bg-red-500 h-[60px] w-[238px]">
+
+        <div className="whiteBracketsRedBoxTop left-1/2 transform -translate-x-1/2 absolute bottom-[47.5%] border-t-2 border-l-2 border-r-2 border-white opacity-70 h-[30px] w-[255px]"></div>
+        <div className="whiteBracketsRedBoxBottom left-1/2 transform -translate-x-1/2 absolute bottom-[43.9%] border-b-2 border-l-2 border-r-2 border-white opacity-70 h-[30px] w-[255px]"></div>
+        <div className="redBox absolute bottom-[43%] left-1/2 transform -translate-x-1/2 -translate-y-1/3 bg-red-500 h-[60px] w-[238px]">
           <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white uppercase font-bold text-sm">
             Watch Live
           </p>
