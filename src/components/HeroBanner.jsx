c656c9e76d66e463cb3a6ce4bf07d474e263d4b2
atlas <a.sproson@outlook.com>
2022-08-10 15:07:09 +0100

    Places video inside HeroBanner, styles it to match reference

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import ReactPlayer from 'react-player'
+
 
 const HeroBanner = () => {
-  const videoUrl = "https://youtu.be/e_E9W2vsRbQ";
+  const videoUrl = "https://assets.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt714eaee50b90fc27/62cc7dcc6a8fb133b0ff7e55/VALORANT_ANNO22_SHATTERED_16x9_27s.mp4";
   return (
-    <div>
+    <div className="-mt-44">
       <div className="vidBackground">
-        <div className="videoLoop border-2 border-red-500">
-            
+        <div className="videoLoop">
+        <ReactPlayer url={videoUrl} playing={true} muted={true} loop={true} width={'100%'} height={'100%'}/>
         </div>
       </div>
     </div>
