4599650851fcc86d90878e0f2601196216e44aef
atlas <a.sproson@outlook.com>
2022-08-13 15:42:47 +0100

    Adds small red box accents

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -5,6 +5,12 @@ const LatestNews = () => {
   return (
     <div>
       <div className="relative">
+
+        <div className="relative flex left-[45%] sm:left-[50%] top-[30px] space-x-11">
+          <div className="bg-red-500 h-2 w-2"></div>
+          <div className="bg-red-500 h-2 w-2"></div>
+        </div>
+
         <div className="stencil">
           <h1 className="text-[200px] uppercase whitespace-nowrap -mt-[40px] -ml-[50px] font-valorant opacity-5 text-center">
             champions
