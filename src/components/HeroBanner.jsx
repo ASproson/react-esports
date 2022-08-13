7df7448a9cf02cb8a02fb4d936e4290a78a65083
atlas <a.sproson@outlook.com>
2022-08-13 19:12:13 +0100

    Fixes redbox issue and hero text styling crash

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -9,21 +9,21 @@ const HeroBanner = () => {
       <div className="whiteLines">
         <div className="whiteBoxLeft absolute left-[32%] transform -translate-x-1/2 h-full w-[310px]">
           <div className="border-l-[1.5px] border-r-[1.5px] w-full h-full opacity-30"></div>
-          <div className="redSquare absolute top-[150px] bg-red-500 h-4 w-4 hidden sm:block"></div>
-          <div className="redTriangle absolute top-[152px] ml-[9px] bg-red-500 h-3 w-3 rotate-45 hidden sm:block"></div>
+          <div className="redSquare absolute top-[150px] sm:top-[250px] md:top-[340px] bg-red-500 h-4 w-4 hidden sm:block"></div>
+          <div className="redTriangle absolute top-[152px] sm:top-[252px] md:top-[342px] ml-[9px] bg-red-500 h-3 w-3 rotate-45 hidden sm:block"></div>
         </div>
 
         <div className="whiteBoxRight absolute transform -translate-x-1/2 right-[14%] h-full w-[200px]">
           <div className="border-l-[1.5px] border-gray-400 w-full h-full opacity-30 z-0"></div>
-          <div className="redSquare absolute top-[150px] bg-red-500 h-4 w-4  sm:hidden"></div>
-          <div className="redTriangle absolute top-[152px] ml-[9px] bg-red-500 h-3 w-3 rotate-45  sm:hidden"></div>
+          <div className="redSquare absolute top-[150px] bg-red-500 h-4 w-4 sm:hidden"></div>
+          <div className="redTriangle absolute top-[152px] ml-[9px] bg-red-500 h-3 w-3 rotate-45 sm:hidden"></div>
         </div>
       </div>
 
       <div className="whiteCutOut absolute -bottom-[58px] -left-[12px] -translate-x-[45px] bg-offWhite h-28 w-28 rotate-45"></div>
 
       <div className="Hero Text">
-        <div className="absolute top-[100px] md:top-[0px] xl:top-[150px] 3xl:top-1/3 transform left-1/2 -translate-x-1/2 ">
+        <div className="absolute top-[100px] md:top-1/3 transform left-1/2 -translate-x-1/2 ">
           <h1 className="font-valorant text-red-500 text-center text-[40px] xl:text-[60px] 3xl:text-[90px] mt-[90px] xl:mt-0 z-20">
             The Latest Events
           </h1>
