c90397df8688efb6433d6b2738f70606e2eb5735
atlas <a.sproson@outlook.com>
2022-08-17 09:21:58 +0100

    Adds more articles

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -56,39 +56,39 @@ const LatestNews = () => {
     },
     {
       id: 4,
-      img: "https://images.contentstack.io/v3/assets/bltb730eada072bdbf4/blt40e206aa5fefb384/62e147e0a30ae37191c61d96/VCT_Stage_2_Masters_Recap_Header.jpg?width=2472&height=1390",
+      img: "https://images.contentstack.io/v3/assets/bltb730eada072bdbf4/blt0efcfb36f6f5201d/62f20db0e71af410b83c113a/article_header_VRL_FINALS_RECAP.jpg?width=2472&height=1390",
       date:
         13 +
         " / " +
         (new Date().getMonth() + 1) +
         " / " +
         new Date().getFullYear(),
-      text: "VCT Stage 2 Masters Recap // Return of the Champions",
-      alt: "championship winners holding trophy",
+      text: "VRL Finals Recap // TÜRKIYE emerges VICTORIOUS",
+      alt: "Valorant regional finals",
     },
     {
       id: 5,
-      img: "https://images.contentstack.io/v3/assets/bltb730eada072bdbf4/bltdad29f65f60eb513/62eb9b7b99dab06ebd13d176/VCT-Off-Season-RedBull-Header_1920x1080.jpg?width=2472&height=1390",
+      img: "https://images.contentstack.io/v3/assets/bltb730eada072bdbf4/blt06251ba6596e1de2/62f6907f5c954177895b10ef/VCT_DL_Article_Cover.jpg?width=2472&height=1390",
       date:
         5 +
         " / " +
         (new Date().getMonth() + 1) +
         " / " +
         new Date().getFullYear(),
-      text: "Red Bull Home Ground // Off-season tour announcement",
-      alt: "championship winners holding trophy",
+      text: "Introducing the valorant challengers '23 // International league details",
+      alt: "Valorant Challengers 2023",
     },
     {
       id: 6,
-      img: "https://images.contentstack.io/v3/assets/bltb730eada072bdbf4/blte641f2bd31733e66/62d6bbc3c75c5836dbd3835c/Valorant_Masters_2022_16x9_V12.jpg?width=2472&height=1390",
+      img: "https://images.contentstack.io/v3/assets/bltb730eada072bdbf4/blt009c099de60029e7/62d5fdab27582a3696ad5e34/VCT22_M2_Finals_Article_Header.jpg?width=2472&height=1390",
       date:
         2 +
         " / " +
         (new Date().getMonth() + 1) +
         " / " +
         new Date().getFullYear(),
-      text: "Valorant Masters // Watch Parties UK & Nordics",
-      alt: "Valorant masters watch parties location maps",
+      text: "Watch Masters Copenhagen // Finals Weekend",
+      alt: "Watch 2023 Valorant Masters tournament",
     },
   ];
 
