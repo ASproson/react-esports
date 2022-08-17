ea274f6d1eb1feb7f840a20b38c567b2b392540b
atlas <a.sproson@outlook.com>
2022-08-17 09:14:31 +0100

    Resolves positioning of articles and show more button

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import "../App.css";
-import { BsChevronDoubleDown } from 'react-icons/bs'
+import { BsChevronDoubleDown } from "react-icons/bs";
 
 const LatestNews = () => {
   const [readMore, setReadMore] = useState(3);
@@ -95,7 +95,7 @@ const LatestNews = () => {
   return (
     <div>
       <div className="relative">
-        <div className=" flex ml-[45%] sm:left-[50%] space-x-12">
+        <div className="flex ml-[45%] sm:left-[50%] space-x-12">
           <div className="bg-red-500 h-2 w-2 absolute top-[30px]"></div>
           <div className="bg-red-500 h-2 w-2 absolute top-[30px]"></div>
         </div>
@@ -112,10 +112,10 @@ const LatestNews = () => {
           </h2>
         </div>
 
-        <section className="Latest Articles relative -top-[145px] ml-5 mr-5 lg:ml-[10px] md:ml-[80px] flex flex-wrap">
+        <section className="Latest Articles lg:w-[1250px] lg:grid lg:grid-cols-3 gap-1 -mt-[140px] relative mx-auto border-2 border-blue-500">
           {articles.slice(0, readMore).map((article) => {
             return (
-              <div key={article.id} className="relative lg:left-[15%] lg:mr-10">
+              <div key={article.id} className="flex justify-center ml-2 mr-2">
                 <div className="relative mb-4">
                   <div className="rounded-sm hover:bg-red-500 duration-200 ease-in lg:h-[230px] lg:w-[400px] md:h-[400px] md:w-[750px]">
                     <img
@@ -134,13 +134,20 @@ const LatestNews = () => {
               </div>
             );
           })}
-          <div className="font-valorantStandard text-3xl cursor-pointer hover:text-red-500 duration-200 ease-in mx-auto" onClick={() => handleReadMoreToggle()}>
+        </section>
+        <div className="Show More Button w-[115px] mx-auto">
+          <div
+            className="font-valorantStandard text-3xl cursor-pointer hover:text-red-500 duration-200 ease-in mx-auto text-center"
+            onClick={() => handleReadMoreToggle()}
+          >
             <button className="">
               {!readMoreClicked ? "Show more" : "Show less"}
             </button>
-            <p className="animate-bounce flex justify-center mt-2"><BsChevronDoubleDown /></p>
+            <p className="animate-bounce flex justify-center mt-2">
+              <BsChevronDoubleDown />
+            </p>
           </div>
-        </section>
+        </div>
       </div>
     </div>
   );
