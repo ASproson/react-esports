b96706567d9ca749f9945acdf396b990db849a82
atlas <a.sproson@outlook.com>
2022-08-17 10:42:10 +0100

    Adds additional styling to art banner

--- src/components/ArtBanner.jsx
+++ src/components/ArtBanner.jsx
@@ -1,13 +1,44 @@
-import React from 'react';
+import React from "react";
+import "../App.css";
 
-const dimensionArtBanner = 'https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blte6088e7599a28fd3/62b4ac17919dfb0f9bf00456/REV_Valorant_2022_E5A1_PlayVALORANT_PlayValorantHomepage_3440x1020_MB02.jpg'
+const dimensionArtBanner =
+  "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blte6088e7599a28fd3/62b4ac17919dfb0f9bf00456/REV_Valorant_2022_E5A1_PlayVALORANT_PlayValorantHomepage_3440x1020_MB02.jpg";
 
 const ArtBanner = () => {
-    return (
-        <div>
-            <img src={dimensionArtBanner}  alt='Valorant Dimension season art banner' className='object-none h-[50vh] mb-20 mt-5'/>
+  return (
+    <div>
+      <img
+        src={dimensionArtBanner}
+        alt="Valorant Dimension season art banner"
+        className="object-none h-[50vh] mb-20 mt-5"
+      />
+      <div className="relative text-white -top-[400px] z-30 uppercase left-[5%] sm:left-[10%] md:left-[21%]">
+        <p className=" font-valorantStandard text-[15px] sm:text-[25px]">
+          E p i s o d e _ 5 / / A c t 1 / / Y r 2
+        </p>
+        <p className=" font-valorantStandard text-[70px] sm:text-[90px] font-bold md:-ml-1">dimension</p>
+
+
+        <div className="WatchLiveButtonContainer absolute left-[2.2%] sm:left-[1.2%] transform top-[140px] sm:top-[170px] hover:cursor-pointer md:-ml-3 lg:-ml-4">
+          <div className="White Brackets">
+            <div className="Top border-t-2 border-l-2 border-r-2 border-white opacity-70 h-[30px] w-[255px] absolute -left-[9px] -top-[10px]"></div>
+            <div className="Bottom border-b-2 border-l-2 border-r-2 border-white opacity-70 h-[30px] w-[255px] absolute -left-[9px] -bottom-[10px]"></div>
+          </div>
+
+          <div className="RedBox bg-red-500 h-[60px] w-[238px]">
+            <p className=" text-white uppercase font-bold text-sm absolute left-1/2 hover:transform -translate-x-1/2 top-[18px] hover:cursor-pointer hover:duration-200 hover:scale-110">
+              Find out more
+            </p>
+            <div className="smallBoxesWithinWatchLiveTopLeft absolute bg-black h-[3px] w-[3px]"></div>
+            <div className="smallBoxesWithinWatchLiveBottomRight absolute bottom-0 right-0 bg-black h-[3px] w-[3px]"></div>
+          </div>
         </div>
-    );
+
+
+        
+      </div>
+    </div>
+  );
 };
 
 export default ArtBanner;
