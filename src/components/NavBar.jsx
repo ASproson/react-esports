2f158e52748dead403b3506b7737de84eec6f90b
atlas <a.sproson@outlook.com>
2022-08-17 09:45:58 +0100

    Adds real logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const NavBar = () => {
+  
   const navList = [
     {
       id: 1,
@@ -24,12 +25,24 @@ const NavBar = () => {
     },
   ];
 
+  const logoLink =
+    "https://img.icons8.com/color/452/valorant.png";
+
   return (
     <div className="navContainer flex bg-black h-20 justify-between sticky z-50">
-      <div className="leftNav flex mr-6 text-white items-center ml-14">Logo</div>
+      <div className="leftNav flex mr-6 text-white items-center ml-14">
+        <img src={logoLink} className="h-10 w-10" />
+      </div>
       <div className="rightNav flex space-x-6 text-[#8FA3B0] items-center mr-14 uppercase font-bold text-sm">
         {navList.map((navEl) => {
-          return <p key={navEl.id} className="cursor-pointer transition duration-200 hover:scale-110 hover:text-white hidden sm:block">{navEl.title}</p>;
+          return (
+            <p
+              key={navEl.id}
+              className="cursor-pointer transition duration-200 hover:scale-110 hover:text-white hidden sm:block"
+            >
+              {navEl.title}
+            </p>
+          );
         })}
       </div>
     </div>
