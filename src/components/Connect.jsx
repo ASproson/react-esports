60bb7cb0adfbf0cb06c015db2d87caa832dacffb
atlas <a.sproson@outlook.com>
2022-08-24 13:23:38 +0100

    Completes VOD connect component

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -51,7 +51,7 @@ const Connect = () => {
   ];
 
   return (
-    <div className="font-valorantStandard -mt-[100px]">
+    <div className="font-valorantStandard -mt-[100px] md:w-[600px] mx-auto">
       {/* Header Text */}
       <div className="flex justify-between pl-6 pr-6 text-3xl">
         <div className="transition ease-in-out hover:text-red-500 hover:scale-105">Vods</div>
