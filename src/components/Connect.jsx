37b8d980c9e34041f949b116b88b9c8c2c7a5d6a
atlas <a.sproson@outlook.com>
2022-09-20 10:31:01 +0100

    Adds wider scale transition boxes for header text

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -45,8 +45,8 @@ const Connect = () => {
     <div className="font-valorantStandard -mt-[100px] md:w-[600px]">
       {/* Header Text */}
       <div className="flex justify-between pl-6 pr-6 text-3xl">
-        <div className="transition ease-in-out hover:text-red-500 hover:scale-105 cursor-pointer">Vods</div>
-        <div className="transition ease-in-out hover:text-red-500 hover:scale-105 cursor-pointer">View All</div>
+        <div className="transition ease-in-out hover:text-red-500 hover:scale-105 cursor-pointer w-full">Vods</div>
+        <div className="transition ease-in-out hover:text-red-500 hover:scale-105 cursor-pointer w-full text-right">View All</div>
       </div>
 
       {/* Team VOD List */}
