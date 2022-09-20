318943fd6b501fc79a857e87bdd2cf162af19b12
atlas <a.sproson@outlook.com>
2022-09-20 10:27:13 +0100

    Adds group hovers for icon and social teaxt

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -41,12 +41,12 @@ const Socials = () => {
             return (
               <div
                 key={link.id}
-                className="flex justify-between mt-4 mb-4 ml-6 mr-6 bg-gray-800 rounded-md h-[80px]"
+                className="flex justify-between mt-4 mb-4 ml-6 mr-6 bg-gray-800 rounded-md h-[80px] group"
               >
                 <div className="flex ml-4 p-4">
                   <div className="flex items-center">
-                      <img src={link.icon} alt="social media icon" className="h-10 w-10 transition ease-in-out hover:scale-110 cursor-pointer" />
-                    <span className="text-2xl mt-1 ml-10 transition ease-in-out hover:text-red-500 hover:scale-110 cursor-pointer">{link.name}</span>
+                    <img src={link.icon} alt="social media icon" className="h-10 w-10 transition ease-in-out group-hover:scale-110 cursor-pointer" />
+                    <span className="text-2xl mt-1 ml-10 transition ease-in-out group-hover:text-red-500 group-hover:scale-110 cursor-pointer">{link.name}</span>
                   </div>
                 </div>
               </div>
