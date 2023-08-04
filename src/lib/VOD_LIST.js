3294af09a672cd9f07bedee9abdf00eece44bec0
atlas <a.sproson@outlook.com>
2023-08-04 17:47:30 +0100

    Extracts list to lib file

---
+++ src/lib/VOD_LIST.js
@@ -0,0 +1,38 @@
+export const VOD_LIST = [
+  {
+    id: 1,
+    leftIcon:
+      "https://am-a.akamaihd.net/image?resize=70:70&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1647451114220_M3C_logo_square.png",
+    rightIcon:
+      "https://am-a.akamaihd.net/image?resize=70:70&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1645434356978_ACEND_VECTOR_PURPLE_WITHOUT_TEXT1.png",
+    leftTeam: "M3C",
+    rightTeam: "Acend",
+  },
+  {
+    id: 2,
+    leftIcon:
+      "https://am-a.akamaihd.net/image?resize=70:70&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1630017626481_kru.png",
+    rightIcon:
+      "https://am-a.akamaihd.net/image?resize=70:70&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1647451114220_M3C_logo_square.png",
+    leftTeam: "KRU Esports",
+    rightTeam: "M3C",
+  },
+  {
+    id: 3,
+    leftIcon:
+      "https://am-a.akamaihd.net/image?resize=70:70&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1637252586938_teamliquid.png",
+    rightIcon:
+      "https://am-a.akamaihd.net/image?resize=70:70&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1645434356978_ACEND_VECTOR_PURPLE_WITHOUT_TEXT1.png",
+    leftTeam: "Team Liquid",
+    rightTeam: "Acend",
+  },
+  {
+    id: 4,
+    leftIcon:
+      "https://am-a.akamaihd.net/image?resize=70:70&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1630017626481_kru.png",
+    rightIcon:
+      "https://am-a.akamaihd.net/image?resize=70:70&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1647451114220_M3C_logo_square.png",
+    leftTeam: "KRU Esports",
+    rightTeam: "FNATIC",
+  },
+];
