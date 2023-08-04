56df1752cfd4304f3b9aae493bfdd0fa28e2e2b0
atlas <a.sproson@outlook.com>
2023-08-04 18:23:36 +0100

    Abstracts nav list for m ap

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,30 +1,8 @@
-import React from "react";
+import { NavElement } from "./NavElement";
+import { NAV_LIST } from "../lib/NAV_LIST";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 export const NavBar = () => {
-  const navList = [
-    {
-      id: 1,
-      title: "schedule",
-    },
-    {
-      id: 2,
-      title: "watch",
-    },
-    {
-      id: 3,
-      title: "standings",
-    },
-    {
-      id: 4,
-      title: "news",
-    },
-    {
-      id: 5,
-      title: new Date().getFullYear() + 1 + " VCT",
-    },
-  ];
-
   const logoLink = "https://img.icons8.com/color/452/valorant.png";
 
   return (
@@ -37,15 +15,8 @@ export const NavBar = () => {
         />
       </div>
       <div className="rightNav flex space-x-6 text-[#8FA3B0] items-center mr-14 uppercase font-bold text-sm">
-        {navList.map((navEl) => {
-          return (
-            <p
-              key={navEl.id}
-              className="cursor-pointer transition duration-200 hover:scale-110 hover:text-white hidden sm:block"
-            >
-              {navEl.title}
-            </p>
-          );
+        {NAV_LIST.map((navEl) => {
+          return <NavElement navEl={navEl} id={navEl.id} />;
         })}
         <GiHamburgerMenu className="h-7 w-7 sm:hidden transition duration-200 hover:scale-125 hover:text-red-500 ease-in cursor-pointer mt-2" />
       </div>
