c30dd6a385b34f820aa081a9ee8775b880acd850
atlas <a.sproson@outlook.com>
2023-08-04 17:30:28 +0100

    Extracts background title to seperate component

---
+++ src/components/ChampionsBackgroundTitle.jsx
@@ -0,0 +1,9 @@
+export const ChampionsBackgroundTitle = () => {
+  return (
+    <div className="Stencil overflow-hidden h-[140px] -mt-[80px]">
+      <h1 className="text-[140px] md:text-[155px] xl:text-[175px] uppercase whitespace-nowrap -ml-[50px] font-valorant opacity-5 text-center">
+        champions are made
+      </h1>
+    </div>
+  );
+};
