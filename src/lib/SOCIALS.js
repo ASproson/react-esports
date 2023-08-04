0dbc951f7e33067ce076dcee5bf30e0e7cf41834
atlas <a.sproson@outlook.com>
2023-08-04 18:09:08 +0100

    Abstracts social links to lib file

---
+++ src/lib/SOCIALS.js
@@ -0,0 +1,22 @@
+export const SOCIALS = [
+  {
+    id: 1,
+    name: "Twitter",
+    icon: "https://assets.valorantesports.com/val/twitter.3c7988bf9d.svg",
+  },
+  {
+    id: 2,
+    name: "Instagram",
+    icon: "https://assets.valorantesports.com/val/instagram.9224bf3ef7.svg",
+  },
+  {
+    id: 3,
+    name: "Youtube",
+    icon: "https://assets.valorantesports.com/val/youtube.f2f774a4bb.svg",
+  },
+  {
+    id: 4,
+    name: "Twitch",
+    icon: "https://assets.valorantesports.com/val/twitch.696cd8ca21.svg",
+  },
+];
