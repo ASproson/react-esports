9a3012ef0749b7cb74f724221e189608f9fa93a2
atlas <a.sproson@outlook.com>
2023-08-04 18:24:47 +0100

    Creates nav element lib

---
+++ src/lib/NAV_LIST.js
@@ -0,0 +1,22 @@
+export const NAV_LIST = [
+  {
+    id: 1,
+    title: "schedule",
+  },
+  {
+    id: 2,
+    title: "watch",
+  },
+  {
+    id: 3,
+    title: "standings",
+  },
+  {
+    id: 4,
+    title: "news",
+  },
+  {
+    id: 5,
+    title: new Date().getFullYear() + 1 + " VCT",
+  },
+];
