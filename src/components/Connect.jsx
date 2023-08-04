8a6aecf2c912613abdde54697fcd5239af830cc4
atlas <a.sproson@outlook.com>
2023-08-04 17:38:55 +0100

    Moves vod list outside of component

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -1,46 +1,45 @@
-import React from "react";
 import { BsPlayCircleFill } from "react-icons/bs";
 
-export const Connect = () => {
-  const vodList = [
-    {
-      id: 1,
-      leftIcon:
-        "https://am-a.akamaihd.net/image?resize=70:70&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1647451114220_M3C_logo_square.png",
-      rightIcon:
-        "https://am-a.akamaihd.net/image?resize=70:70&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1645434356978_ACEND_VECTOR_PURPLE_WITHOUT_TEXT1.png",
-      leftTeam: "M3C",
-      rightTeam: "Acend",
-    },
-    {
-      id: 2,
-      leftIcon:
-        "https://am-a.akamaihd.net/image?resize=70:70&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1630017626481_kru.png",
-      rightIcon:
-        "https://am-a.akamaihd.net/image?resize=70:70&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1647451114220_M3C_logo_square.png",
-      leftTeam: "KRU Esports",
-      rightTeam: "M3C",
-    },
-    {
-      id: 3,
-      leftIcon:
-        "https://am-a.akamaihd.net/image?resize=70:70&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1637252586938_teamliquid.png",
-      rightIcon:
-        "https://am-a.akamaihd.net/image?resize=70:70&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1645434356978_ACEND_VECTOR_PURPLE_WITHOUT_TEXT1.png",
-      leftTeam: "Team Liquid",
-      rightTeam: "Acend",
-    },
-    {
-      id: 4,
-      leftIcon:
-        "https://am-a.akamaihd.net/image?resize=70:70&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1630017626481_kru.png",
-      rightIcon:
-        "https://am-a.akamaihd.net/image?resize=70:70&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1647451114220_M3C_logo_square.png",
-      leftTeam: "KRU Esports",
-      rightTeam: "FNATIC",
-    },
-  ];
+const vodList = [
+  {
+    id: 1,
+    leftIcon:
+      "https://am-a.akamaihd.net/image?resize=70:70&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1647451114220_M3C_logo_square.png",
+    rightIcon:
+      "https://am-a.akamaihd.net/image?resize=70:70&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1645434356978_ACEND_VECTOR_PURPLE_WITHOUT_TEXT1.png",
+    leftTeam: "M3C",
+    rightTeam: "Acend",
+  },
+  {
+    id: 2,
+    leftIcon:
+      "https://am-a.akamaihd.net/image?resize=70:70&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1630017626481_kru.png",
+    rightIcon:
+      "https://am-a.akamaihd.net/image?resize=70:70&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1647451114220_M3C_logo_square.png",
+    leftTeam: "KRU Esports",
+    rightTeam: "M3C",
+  },
+  {
+    id: 3,
+    leftIcon:
+      "https://am-a.akamaihd.net/image?resize=70:70&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1637252586938_teamliquid.png",
+    rightIcon:
+      "https://am-a.akamaihd.net/image?resize=70:70&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1645434356978_ACEND_VECTOR_PURPLE_WITHOUT_TEXT1.png",
+    leftTeam: "Team Liquid",
+    rightTeam: "Acend",
+  },
+  {
+    id: 4,
+    leftIcon:
+      "https://am-a.akamaihd.net/image?resize=70:70&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1630017626481_kru.png",
+    rightIcon:
+      "https://am-a.akamaihd.net/image?resize=70:70&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1647451114220_M3C_logo_square.png",
+    leftTeam: "KRU Esports",
+    rightTeam: "FNATIC",
+  },
+];
 
+export const Connect = () => {
   return (
     <div className="font-valorantStandard -mt-[100px] md:w-[600px]">
       {/* Header Text */}
