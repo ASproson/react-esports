30d3f8ae4f555f7b96b10ef4d05e80855b0f975d
atlas <a.sproson@outlook.com>
2024-01-27 18:30:29 +0000

    Uses articles over divs

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,6 +1,6 @@
 export const Article = ({ article, id }) => {
   return (
-    <div key={id} className="flex justify-center ml-2 mr-2">
+    <article key={id} className="flex justify-center ml-2 mr-2">
       <div className="relative mb-4">
         <div className="rounded-sm hover:bg-red-500 duration-200 ease-in lg:h-[230px] lg:w-[400px] md:h-[300px] md:w-[600px]">
           <img
@@ -10,12 +10,12 @@ export const Article = ({ article, id }) => {
           />
         </div>
         <div className="font-valorantStandard">
-          <div className="mt-3 text-2xl">{article.date}</div>
-          <div className="text-3xl cursor-pointer hover:text-red-500 duration-200 ease-in lg:w-[400px]">
+          <p className="mt-3 text-2xl">{article.date}</p>
+          <h3 className="text-3xl cursor-pointer hover:text-red-500 duration-200 ease-in lg:w-[400px]">
             {article.text}
-          </div>
+          </h3>
         </div>
       </div>
-    </div>
+    </article>
   );
 };
