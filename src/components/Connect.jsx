b25cc8ab6429563c5c344f65e49754ac4689bc51
atlas <a.sproson@outlook.com>
2024-01-27 18:33:22 +0000

    Tidies up component

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -3,25 +3,29 @@ import { VOD_LIST } from "../lib/VOD_LIST";
 
 export const Connect = () => {
   return (
-    <div className="font-valorantStandard -mt-[100px] md:w-[600px]">
-      {/* Header Text */}
+    <section className="font-valorantStandard -mt-[100px] md:w-[600px]">
       <div className="flex justify-between pl-6 pr-6 text-3xl">
-        <div className="transition ease-in-out hover:text-red-500 hover:scale-105 cursor-pointer w-full">
+        <h3 className="transition ease-in-out hover:text-red-500 hover:scale-105 cursor-pointer w-full">
           Vods
-        </div>
-        <div className="transition ease-in-out hover:text-red-500 hover:scale-105 cursor-pointer w-full text-right">
+        </h3>
+        <h3 className="transition ease-in-out hover:text-red-500 hover:scale-105 cursor-pointer w-full text-right">
           View All
-        </div>
+        </h3>
       </div>
 
-      {/* Team VOD List */}
-      <div className="flex justify-between text-white">
-        <div className="w-full md:w-[600px]">
-          {VOD_LIST.map((match) => {
-            return <VodCard match={match} key={match.id} />;
-          })}
-        </div>
+      <TeamVodList />
+    </section>
+  );
+};
+
+const TeamVodList = () => {
+  return (
+    <article className="flex justify-between text-white">
+      <div className="w-full md:w-[600px]">
+        {VOD_LIST.map((match) => {
+          return <VodCard match={match} key={match.id} />;
+        })}
       </div>
-    </div>
+    </article>
   );
 };
