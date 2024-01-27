341402abd3f91c790165323e9086625dd0b9f5b0
atlas <a.sproson@outlook.com>
2024-01-27 18:00:56 +0000

    Adds pb/pt for responsiveness

--- src/components/NavElement.jsx
+++ src/components/NavElement.jsx
@@ -2,7 +2,7 @@ export const NavElement = ({ navEl, id }) => {
   return (
     <span
       key={id}
-      className="cursor-pointer transition duration-200 hover:scale-110 hover:text-white hidden sm:block"
+      className="cursor-pointer transition duration-200 hover:scale-110 hover:text-white hidden sm:block pb-2 pt-2"
     >
       {navEl.title}
     </span>
